053cea8ef31258d89441c09e32fabcdab15e87e1
Dave Syer <dsyer@pivotal.io>
2017-03-20 14:14:31 +0000

    Remove unused thymeleaf config

--- core/src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ core/src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -8,8 +8,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.samples.petclinic.system.ThymeleafTemplateConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,7 +27,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(OwnerController.class)
-@Import(ThymeleafTemplateConfiguration.class)
 public class OwnerControllerTests {
 
     private static final int TEST_OWNER_ID = 1;
